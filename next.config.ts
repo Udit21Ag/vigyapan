068ddc3b76d46a9a306f7ca36d89259319191f7f
neutron-69 <mohul.batra@gmail.com>
2025-09-11 18:12:16 +0530

    A

--- next.config.ts
+++ next.config.ts
@@ -32,23 +32,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups'
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy', 
-            value: 'credentialless'
-          }
-        ],
-      },
-    ]
-  },
 };
 
 export default nextConfig;
